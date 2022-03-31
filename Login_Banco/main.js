48418a94dd1bb70fc202a5f8b31c31fca532f415
Manuel Sebastian <seferinoubago12@gmail.com>
2022-03-30 20:02:46 -0600

    Login100

--- Login_Banco/main.js
+++
@@ -1,6 +0,0 @@
- var cuentas = [
-    {Nombre: "Marcos Vleeschower", Saldo: 300, Password:"Spartan17", Usuario:"Vleeschowder"},
-    {Nombre: "Manuel Ramos", Saldo: 666, Password:"yungtaco420", Usuario:"PimpTaco"},
-    {Nombre: "Salvador Soto", Saldo: 420, Password:"bigsoto69", Usuario:"TheSotoKing"}
-
-]
